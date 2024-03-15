99794a4526959b76bbd27f597069709bf05a9958
João Victor <123279954+joaodev2005@users.noreply.github.com>
2024-03-15 19:53:14 -0300

    Iniciando no TS

---
+++ 1_INTRO/index.js
@@ -0,0 +1,10 @@
+var firstName = 'João';
+var lastName = "Magnun";
+var anotherName = 1;
+var x = true;
+function greeting(name) {
+    console.log("Hello ".concat(name));
+}
+greeting(firstName);
+greeting(x);
+greeting(anotherName);
---
+++ 1_INTRO/index.ts
@@ -0,0 +1,12 @@
+const firstName = 'João';
+const lastName = "Magnun";
+const anotherName = 1;
+const x = true;
+
+function greeting(name: string) {
+    console.log(`Hello ${name}`);
+}
+
+greeting(firstName);
+greeting(x);
+greeting(anotherName);
