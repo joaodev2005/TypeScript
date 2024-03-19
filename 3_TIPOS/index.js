f9bc1fb27b23b6e9353d4ac70c7b781a83130aa0
João Victor <123279954+joaodev2005@users.noreply.github.com>
2024-03-19 19:32:20 -0300

    arrays

---
+++ 3_TIPOS/index.js
@@ -0,0 +1,11 @@
+"use strict";
+// 1 - arrays
+let numbers = [1, 2, 3];
+numbers.push(4);
+numbers.push(5);
+// console.log(numbers);
+// console.log(numbers[3]);
+const names = ["João", "Maria"];
+names.push("Joaquim");
+names.push("Pedro");
+console.log(names);
---
+++ 3_TIPOS/index.ts
@@ -0,0 +1,18 @@
+// 1 - arrays
+
+let numbers: number[] = [1, 2, 3];
+
+numbers.push(4);
+
+numbers.push(5);
+
+// console.log(numbers);
+
+// console.log(numbers[3]);
+
+const names: string[] = ["João", "Maria"];
+
+names.push("Joaquim");
+names.push("Pedro");
+
+console.log(names)
