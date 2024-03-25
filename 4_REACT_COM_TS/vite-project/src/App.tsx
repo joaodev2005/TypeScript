45a7580abff365c57a08b0ca2051d7c7eb8d2beb
João Victor <123279954+joaodev2005@users.noreply.github.com>
2024-03-25 19:05:48 -0300

    variaveis com ts e react

--- 4_REACT_COM_TS/vite-project/src/App.tsx
+++ 4_REACT_COM_TS/vite-project/src/App.tsx
@@ -3,11 +3,19 @@ import './App.css'
 
 function App() {
 
+  // 1 - variaveis
+
+  const name: string = 'Joaquim'
+  const age: number = 23
+  const isWorking: boolean = false
 
   return (
     <>
       <div className="App">
         <h1>TS com React</h1>
+        <h2>Nome: {name}</h2>
+        <p>Idade: {age}</p>
+        <p>Esta trabalhando: {isWorking ? 'Sim' : 'Nao'}</p>
       </div>
     </>
   )
