38fdb0aaf699818e94c7911ff3a70f61d8348319
João Victor <123279954+joaodev2005@users.noreply.github.com>
2024-04-03 19:44:27 -0300

    lembrando union types

---
+++ CFB_CURSOS_TS/build/aula04.js
@@ -0,0 +1,9 @@
+"use strict";
+let vTeste = 'teste';
+let cursos = ['Spring', 'TypeScript'];
+let valores = [1, 2, 3];
+cursos.push('Angular');
+valores.push(4);
+console.log(cursos);
+console.log(valores);
+console.log(vTeste);
---
+++ CFB_CURSOS_TS/src/aula04.ts
@@ -0,0 +1,14 @@
+// let vTeste: string | number | boolean = 'teste';
+
+let vTeste: any = 'teste';
+
+let cursos: string[] = ['Spring', 'TypeScript'];
+
+let valores: number[] = [1, 2, 3];
+
+cursos.push('Angular');
+valores.push(4);
+
+console.log(cursos)
+console.log(valores)
+console.log(vTeste)
