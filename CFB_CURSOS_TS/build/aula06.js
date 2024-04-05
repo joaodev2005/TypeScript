9d0099ee31f14d02d5e70dd3bc4a979b5c4f524e
João Victor <123279954+joaodev2005@users.noreply.github.com>
2024-04-05 19:24:15 -0300

    tuplas

---
+++ CFB_CURSOS_TS/build/aula06.js
@@ -0,0 +1,4 @@
+"use strict";
+let coisas = ['GTR R34', 1, true, 'Ferrari'];
+console.log(coisas);
+console.log(coisas[0]);
---
+++ CFB_CURSOS_TS/src/aula06.ts
@@ -0,0 +1,4 @@
+let coisas: [string, number, boolean, string] = ['GTR R34', 1, true, 'Ferrari'];
+
+console.log(coisas)
+console.log(coisas[0])
