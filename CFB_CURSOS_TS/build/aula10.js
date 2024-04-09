11016f383646eeed527d058d42de94e2336d8403
João Victor <123279954+joaodev2005@users.noreply.github.com>
2024-04-09 20:01:13 -0300

    up

---
+++ CFB_CURSOS_TS/build/aula10.js
@@ -0,0 +1,8 @@
+"use strict";
+let nValor;
+let sValor;
+let bValor;
+bValor = 10;
+nValor = bValor;
+console.log(bValor);
+console.log(typeof bValor);
---
+++ CFB_CURSOS_TS/src/aula10.ts
@@ -0,0 +1,10 @@
+let nValor: number;
+let sValor: string;
+let bValor: unknown;
+
+bValor = 10;
+
+nValor = <number>bValor;
+
+console.log(bValor)
+console.log(typeof bValor)
