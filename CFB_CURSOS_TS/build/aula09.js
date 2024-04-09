8476964897c050a8403f3890ede728989925dd00
João Victor <123279954+joaodev2005@users.noreply.github.com>
2024-04-09 18:59:53 -0300

    Undefined - Unknow - Null

--- CFB_CURSOS_TS/build/aula09.js
+++ CFB_CURSOS_TS/build/aula09.js
@@ -2,3 +2,8 @@
 let vNome;
 vNome = null;
 console.log(vNome);
+let vNome2;
+console.log(vNome2);
+let vNome3 = 10;
+let vNum = vNome3;
+console.log(vNum);
--- CFB_CURSOS_TS/src/aula09.ts
+++ CFB_CURSOS_TS/src/aula09.ts
@@ -1,5 +1,15 @@
 let vNome: string | null;
-
 vNome = null;
 
 console.log(vNome)
+
+
+let vNome2;
+
+console.log(vNome2)
+
+
+let vNome3: unknown = 10;
+let vNum: unknown  = vNome3;
+
+console.log(vNum)
