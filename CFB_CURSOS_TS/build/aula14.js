d5e87075272e01d158e3eda05e598daad6562bf5
João Victor <123279954+joaodev2005@users.noreply.github.com>
2024-04-12 19:25:52 -0300

    up

--- CFB_CURSOS_TS/build/aula14.js
+++ CFB_CURSOS_TS/build/aula14.js
@@ -7,3 +7,12 @@ const fSoma = (...n) => {
     return a;
 };
 console.log(fSoma(10, 20, 40, 88));
+const mediaEscola = (...n) => {
+    let z = 0;
+    n.forEach((e) => {
+        z += e;
+    });
+    return z / n.length;
+};
+let media = mediaEscola(5, 4, 3, 2);
+console.log('Sua média escolar é:', +media);
--- CFB_CURSOS_TS/src/aula14.ts
+++ CFB_CURSOS_TS/src/aula14.ts
@@ -11,3 +11,18 @@ const fSoma = (...n: number[]) => {
 }
 
 console.log(fSoma(10, 20, 40, 88))
+
+const mediaEscola = (...n: number[]) => {
+    let z: number = 0
+
+    n.forEach((e: number) => {
+
+        z += e
+    })
+
+    return z / n.length
+}
+
+let media = mediaEscola(5, 4, 3, 2)
+
+console.log('Sua média escolar é:', + media)
