aba7b8222dadc064f215cf862a42a872e6b6c2db
João Victor <123279954+joaodev2005@users.noreply.github.com>
2024-04-22 19:10:47 -0300

    Interfaces

---
+++ CFB_CURSOS_TS/src/aula26.ts
@@ -0,0 +1,14 @@
+interface curso{
+    title: string;
+    duration: number;
+    year: number;
+    genres: string[]
+}
+
+let movie1: curso
+let movie2: curso
+
+movie1 = { title: 'Star Wars', duration: 2, year: 2000, genres: ['Action', 'Sci-Fi'] }
+movie2 = { title: 'Pulp Fiction', duration: 2, year: 1996, genres: ['Comedy', 'Thriller'] }
+
+console.log(movie1)
