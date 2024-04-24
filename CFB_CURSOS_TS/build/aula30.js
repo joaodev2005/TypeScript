25af258ebf7bf1a00eaed4c2ab4775c1d631a6a2
João Victor <123279954+joaodev2005@users.noreply.github.com>
2024-04-24 20:02:07 -0300

    Namespace + Classes

--- CFB_CURSOS_TS/build/aula30.js
+++ CFB_CURSOS_TS/build/aula30.js
@@ -1,15 +1,57 @@
 "use strict";
 var Veiculos;
 (function (Veiculos) {
+    let Cores;
+    (function (Cores) {
+        Cores[Cores["preto"] = 0] = "preto";
+        Cores[Cores["branco"] = 1] = "branco";
+        Cores[Cores["vermelho"] = 2] = "vermelho";
+        Cores[Cores["azul"] = 3] = "azul";
+        Cores[Cores["amarelo"] = 4] = "amarelo";
+        Cores[Cores["verde"] = 5] = "verde";
+        Cores[Cores["cinza"] = 6] = "cinza";
+    })(Cores || (Cores = {}));
     class Carro {
         name;
         motor;
-        constructor(name) {
+        cor;
+        constructor(name, motor, cor) {
             this.name = name;
-            this.motor = new motores.Motor('2.0');
+            this.motor = motor;
+            this.cor = Cores[cor];
+        }
+        ligar() {
+            this.motor.liga = true;
+        }
+        desligar() {
+            this.motor.liga = false;
+        }
+        get minhaCor() {
+            return this.cor;
+        }
+        get meuNome() {
+            return this.name;
+        }
+        get estouLigado() {
+            return (this.motor.liga ? 'sim' : 'nao');
+        }
+        get minhaPotencia() {
+            return this.motor.potencia;
         }
     }
     Veiculos.Carro = Carro;
+    class CarroEsportivo extends Carro {
+        constructor(name, cor) {
+            super(name, new motores.Motor('2.0', 4, new motores.Turbo(100)), 0);
+        }
+    }
+    Veiculos.CarroEsportivo = CarroEsportivo;
+    class CarroPopular extends Carro {
+        constructor(name, cor) {
+            super(name, new motores.Motor('1.0', 4), 3);
+        }
+    }
+    Veiculos.CarroPopular = CarroPopular;
 })(Veiculos || (Veiculos = {}));
 var motores;
 (function (motores) {
@@ -18,15 +60,40 @@ var motores;
         constructor(pot) {
             this.pot = pot;
         }
+        get potencia() {
+            return this.pot;
+        }
     }
+    motores.Turbo = Turbo;
     class Motor {
+        ligado;
+        cilindros;
         pot;
-        constructor(pot) {
-            this.pot = pot;
+        constructor(pot, cilindros, turbo) {
+            this.ligado = false;
+            this.cilindros = cilindros;
+            this.pot = pot + (turbo ? turbo.potencia : 0);
+        }
+        set liga(ligado) {
+            this.ligado = ligado;
+        }
+        get liga() {
+            return this.ligado;
+        }
+        get potencia() {
+            return this.pot;
         }
     }
     motores.Motor = Motor;
 })(motores || (motores = {}));
-const carro = new Veiculos.Carro('Ferrari');
-console.log(carro.name);
-console.log(carro.motor);
+const carro1 = new Veiculos.CarroPopular('supra', 4);
+const carro2 = new Veiculos.CarroEsportivo('Ford Mustang GT', 1);
+console.log(`Nome: ${carro1.meuNome}`);
+console.log(`Cor: ${carro1.minhaCor}`);
+console.log(`Potencia: ${carro1.minhaPotencia}`);
+console.log(`Ligado: ${carro1.estouLigado}`);
+console.log('==========================');
+console.log(`Nome: ${carro2.meuNome}`);
+console.log(`Cor: ${carro2.minhaCor}`);
+console.log(`Potencia: ${carro2.minhaPotencia}`);
+console.log(`Ligado: ${carro2.estouLigado}`);
--- CFB_CURSOS_TS/src/aula30.ts
+++ CFB_CURSOS_TS/src/aula30.ts
@@ -1,30 +1,101 @@
 namespace Veiculos {
-    export class Carro {
-        name: string
-        motor: motores.Motor
-        constructor(name: string) {
+    enum Cores { 'preto', 'branco', 'vermelho', 'azul', 'amarelo', 'verde', 'cinza' }
+    export abstract class Carro {
+        private name: string
+        private motor: motores.Motor
+        private cor: string
+        constructor(name: string, motor: motores.Motor, cor: Cores) {
             this.name = name
-            this.motor = new motores.Motor('2.0')
+            this.motor = motor
+            this.cor = Cores[cor]
+        }
+
+        public ligar() {
+            this.motor.liga = true
+        }
+
+        public desligar() {
+            this.motor.liga = false
+        }
+
+        get minhaCor() {
+            return this.cor
+        }
+
+        get meuNome() {
+            return this.name
+        }
+
+        get estouLigado() {
+            return (this.motor.liga ? 'sim' : 'nao')
+        }
+
+        get minhaPotencia() {
+            return this.motor.potencia
+        }
+    }
+
+    export class CarroEsportivo extends Carro {
+        constructor(name: string, cor: Cores) {
+            super(name, new motores.Motor('2.0', 4, new motores.Turbo(100)), 0)
+        }
+    }
+
+    export class CarroPopular extends Carro {
+        constructor(name: string, cor: Cores) {
+            super(name, new motores.Motor('1.0', 4), 3)
         }
     }
 }
 
+
+
 namespace motores {
-    class Turbo {
-        pot: number
+    export class Turbo {
+        private pot: number
         constructor(pot: number) {
             this.pot = pot
         }
+
+        get potencia() {
+            return this.pot
+        }
     }
     export class Motor {
-        pot: string
-        constructor(pot: string) {
-            this.pot = pot
+        private ligado: boolean
+        private cilindros: number
+        private pot: string
+        constructor(pot: string, cilindros: number, turbo?: Turbo) {
+            this.ligado = false
+            this.cilindros = cilindros
+            this.pot = pot + (turbo ? turbo.potencia : 0)
+        }
+
+        set liga(ligado: boolean) {
+            this.ligado = ligado
+        }
+
+        get liga() {
+            return this.ligado
+        }
+
+        get potencia() {
+            return this.pot
         }
     }
 }
 
-const carro = new Veiculos.Carro('Ferrari');
+const carro1 = new Veiculos.CarroPopular('supra', 4)
+const carro2 = new Veiculos.CarroEsportivo('Ford Mustang GT', 1)
 
-console.log(carro.name)
-console.log(carro.motor)
+console.log(`Nome: ${carro1.meuNome}`)
+console.log(`Cor: ${carro1.minhaCor}`)
+console.log(`Potencia: ${carro1.minhaPotencia}`)
+console.log(`Ligado: ${carro1.estouLigado}`)
+
+console.log('==========================')
+
+console.log(`Nome: ${carro2.meuNome}`)
+console.log(`Cor: ${carro2.minhaCor}`)
+console.log(`Potencia: ${carro2.minhaPotencia}`)
+console.log(`Ligado: ${carro2.estouLigado}`)
