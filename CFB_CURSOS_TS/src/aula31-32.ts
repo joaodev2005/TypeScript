11d565cb0ae5b380d3858f06cc63cc189a54a101
João Victor <123279954+joaodev2005@users.noreply.github.com>
2024-04-29 19:04:59 -0300

    Desestruturação

---
+++ CFB_CURSOS_TS/src/aula31-32.ts
@@ -0,0 +1,32 @@
+// let av = [10, 20, 30, 40]
+
+// let [ aa, bb, cc, dd] = av
+
+// console.log(aa)
+// console.log(bb)
+// console.log(cc)
+// console.log(dd)
+
+
+// let [aa, bb, cc, dd] = ['branco', 'vermelho', 'azul', 'amarelo']
+
+// console.log(aa)
+// console.log(bb)
+// console.log(cc)
+// console.log(dd) 
+
+// const games = {
+//     jogo1: 'The last of Us 2',
+//     jogo2: 'Cuphead',
+//     jogo3: 'Red Dead Redemption 2',
+//     jogo4: 'GTA V',
+//     jogo5: 'God of War 4'
+// }
+
+// let { jogo1, jogo2, jogo3, jogo4, jogo5 } = games
+
+// console.log(jogo1)
+// console.log(jogo2)
+// console.log(jogo3)
+// console.log(jogo4)
+// console.log(jogo5)
