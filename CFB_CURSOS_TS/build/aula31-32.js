71c06cee2ed324d897a7c77ac392f48eb880edf5
João Victor <123279954+joaodev2005@users.noreply.github.com>
2024-04-29 19:26:13 -0300

    finalizando sobre Desestruturação

--- CFB_CURSOS_TS/build/aula31-32.js
+++ CFB_CURSOS_TS/build/aula31-32.js
@@ -1,1 +1,15 @@
 "use strict";
+let [nu1, nu2 = 0, nu3 = 0, nu4 = 0, ...nu5] = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+const fColors = () => {
+    return ['branco', 'vermelho', 'azul', 'amarelo'];
+};
+let [co1, co2, co3, co4] = fColors();
+console.log(co1);
+console.log(co2);
+console.log(co3);
+console.log(co4);
+let text = 'Curso de TypeScript';
+let [p1, p2, p3] = text.split(" ");
+console.log(p1);
+console.log(p2);
+console.log(p3);
--- CFB_CURSOS_TS/src/aula31-32.ts
+++ CFB_CURSOS_TS/src/aula31-32.ts
@@ -30,3 +30,30 @@
 // console.log(jogo3)
 // console.log(jogo4)
 // console.log(jogo5)
+
+let [nu1, nu2=0, nu3=0, nu4=0, ...nu5] = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+
+// console.log(nu1)
+// console.log(nu2)
+// console.log(nu3)
+// console.log(nu4)
+// console.log(nu5)
+
+const fColors = () => {
+    return ['branco', 'vermelho', 'azul', 'amarelo']
+} 
+
+let [co1, co2, co3, co4] = fColors()
+
+console.log(co1)
+console.log(co2)
+console.log(co3)
+console.log(co4)
+
+let text = 'Curso de TypeScript'
+
+let [p1, p2, p3] = text.split(" ")
+
+console.log(p1)
+console.log(p2)
+console.log(p3)
